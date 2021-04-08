27efc5fd507ccc7c44901196c865b24d1b0acd42
Saiful islam <saifulkapasia23@gmail.com>
2021-04-08 22:34:37 +0600

    Added header components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header/Header';
+import Box from './components/Box/Box';
 
 function App() {
   return (
     <div className="App">
-      
+      <Container>
+        <Header />
+        <Box />
+      </Container>
     </div>
   );
 }
